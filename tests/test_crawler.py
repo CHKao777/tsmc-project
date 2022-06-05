c8a2dd3dad3e2d753759a8f12c5bb5cc37caa246
jayzhan211 <jayzhan211@gmail.com>
2022-06-05 21:43:47 +0800

    add more test
    Signed-off-by: jayzhan211 <jayzhan211@gmail.com>

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,5 +1,7 @@
 from datetime import date
-from crawler.GoogleCrawler import add_new_date
+import datetime
+import crawler.GoogleCrawler as gcrawler
+
 
 def test_mongodb_log_basic(mongodb):
     assert 'crawler_logs' in mongodb.list_collection_names()
@@ -11,7 +13,7 @@ def test_add_new_date(mongodb):
 
     start_date = date.fromisoformat('2022-06-01')
 
-    add_new_date(mongodb.crawler_logs, 1, start_date)
+    gcrawler.add_new_date(mongodb.crawler_logs, 1, start_date)
     log = mongodb.crawler_logs.find_one({'Date': '2022-06-01'})
     assert log
     log = mongodb.crawler_logs.find_one({'Date': '2022-06-03'})
@@ -19,7 +21,21 @@ def test_add_new_date(mongodb):
 
     start_date = date.fromisoformat('2022-06-03')
 
-    add_new_date(mongodb.crawler_logs, 1, start_date)
+    gcrawler.add_new_date(mongodb.crawler_logs, 1, start_date)
     log = mongodb.crawler_logs.find_one({'Date': '2022-06-03'})
     assert log
     assert log['Status'] == 'undone'
+
+def test_get_13_days():
+    day = datetime.date(2022, 6, 4)
+    assert gcrawler.get_13_days(day) == datetime.date(2022, 5, 22)
+    day = datetime.date(2022, 6, 5)
+    assert gcrawler.get_13_days(day) == datetime.date(2022, 5, 23)
+    day = datetime.date(2022, 6, 6)
+    assert gcrawler.get_13_days(day) == datetime.date(2022, 5, 24)
+    day = datetime.date(2022, 6, 7)
+    assert gcrawler.get_13_days(day) == datetime.date(2022, 5, 25)
+    day = datetime.date(2022, 6, 8)
+    assert gcrawler.get_13_days(day) == datetime.date(2022, 5, 26)
+    day = datetime.date(2022, 1, 9)
+    assert gcrawler.get_13_days(day) == datetime.date(2021, 12, 27)
